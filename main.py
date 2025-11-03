552a6ab296daba3775fc5edebfe5b87e19711c4b
IJQShrestha <shrestha.iq@obmontessori.edu.ph>
2025-11-03 14:30:15 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from pyscript import document, display
+subjects = ["Science", "English", "ICT", "Math", "Filipino", "PE"]
+units = (5, 5, 2, 5, 3, 1)
+
+def calculate_gwa(e):
+    fname = document.getElementById("fname").value
+    lname = document.getElementById("lname").value
+
+
